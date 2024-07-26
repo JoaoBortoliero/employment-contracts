f9d3fb9f1ffd2c5b6e00898c81161e0a840bcf81
João Vitor <bortoliero.10@hotmail.com>
2024-07-26 16:12:57 -0300

    Create employment contracts project

---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace EmploymentContracts.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ Entities/Worker.cs
@@ -0,0 +1,46 @@
+﻿using EmploymentContracts.Entities.Enums;
+
+namespace EmploymentContracts.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }  // Associação entre duas classes diferentes (Composição de objetos)
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>(); // Um trabalhador pode ter vários contratos de trabalhos
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary; // O trabalhador irá ganhar o trabalho base em todos os casos
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                    sum += contract.TotalValue();
+            }
+            return sum;
+        }
+    }
+}
